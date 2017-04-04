3efb34bccd4e8802184acf2a216e353a2ca0bfe7
SunTzu <171946@supinfo.com>
2017-04-04 12:21:48 +0200

    separation composant date

--- src/BaseComponent/UpDate.tsx
+++ src/BaseComponent/UpDate.tsx
@@ -3,33 +3,38 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { UpFormControl } from "../UpForm/UpFormControl"
 import { TypeDateControl, dateFormat } from "../ControlError/TypeDateControl"
+import UpDate from "../externalReactComponent/UpDate"
 
-export default class UpDate extends UpFormControl<Date> {
+
+export default class UpDateComp extends UpFormControl<Date> {
     inputElementGroup: HTMLDivElement;    constructor(p, c) {        super(p, c);
         this._ControlErrorCentral.addControl(new TypeDateControl(dateFormat.date));
-    }    setInput(data) {        $(this.inputElementGroup).data("DateTimePicker").date(data);    }    _componentDidMount() {
-        $(this.inputElementGroup).datetimepicker({ locale: 'fr', format: "DD/MM/YYYY" });
-        $(this.inputElementGroup).on("dp.change", this.handleChangeJsEventGlobal);
+    }    setInput(data) {       // $(this.inputElementGroup).data("DateTimePicker").date(data);    }    _componentDidMount() {
+        //$(this.inputElementGroup).datetimepicker({ locale: 'fr', format: "DD/MM/YYYY" });
+        //$(this.inputElementGroup).on("dp.change", this.handleChangeJsEventGlobal);
     }    renderField() {
 
-        return <div className='input-group date' style={{ marginBottom: "3px" }} ref={(input) => { this.inputElementGroup = input; } } >
-            <input
-                ref={(input) => { this.inputElement = input; } }
-                style={this.state.hasError === true ? { borderColor: "red" } : null}
-                type='text'
-                className="form-control" />
-            <span className="input-group-addon">
-                <span className="glyphicon glyphicon-calendar"></span>
-            </span>
-        </div>
+        return <UpDate onChange={this.handleChangeEventGlobal} hasError={this.state.hasError} isNuallble={this.isNuallble}></UpDate>
+
+        //return <div className='input-group date' style={{ marginBottom: "3px" }} ref={(input) => { this.inputElementGroup = input; } } >
+        //    <input
+        //        ref={(input) => { this.inputElement = input; } }
+        //        style={this.state.hasError === true ? { borderColor: "red" } : null}
+        //        type='text'
+        //        className="form-control" />
+        //    <span className="input-group-addon">
+        //        <span className="glyphicon glyphicon-calendar"></span>
+        //    </span>
+        //</div>
 
     }
 
-    handleChangeJsEvent(event: any) {
-        if (typeof (event.date) === "object" && event.date && typeof (event.date.toDate) === "function") {
-            return event.date.toDate();
-        }
-        return null;
+    handleChangeJsEvent(event: Date) {
+        //if (typeof (event.date) === "object" && event.date && typeof (event.date.toDate) === "function") {
+        //    return event.date.toDate();
+        //}
+        //return null;
+        return event;
     }
 
     isEmpty(value) {
