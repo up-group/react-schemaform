a8ad0e185fbd12b38bc8e36103c4c00d9dddbfec
SunTzu <171946@supinfo.com>
2017-04-04 14:24:48 +0200

    separation datetime

--- src/BaseComponent/UpDateTime.tsx
+++ src/BaseComponent/UpDateTime.tsx
@@ -2,34 +2,37 @@
 import * as React from "react";
 import {UpFormControl} from "../UpForm/UpFormControl"
 import {TypeDateControl, dateFormat} from "../ControlError/TypeDateControl"
-import "eonasdan-bootstrap-datetimepicker"
+import UpDateTime from "../externalReactComponent/UpDateTime"
 
-export default class UpDateTime extends UpFormControl<Date> {
+export default class UpDateTimeComp extends UpFormControl<Date> {
     inputElementGroup: HTMLDivElement;    constructor(p, c) {        super(p, c);
         this._ControlErrorCentral.addControl(new TypeDateControl(dateFormat.dateTime));
-    }    setInput(data) {        $(this.inputElementGroup).data("DateTimePicker").date(data);    }
+    }    setInput(data) {     //   $(this.inputElementGroup).data("DateTimePicker").date(data);    }
     _componentDidMount() {
-        $(this.inputElementGroup).datetimepicker({ locale: 'fr', format: "DD/MM/YYYY HH:mm" });
-        $(this.inputElementGroup).on("dp.change", this.handleChangeJsEventGlobal);
+        //$(this.inputElementGroup).datetimepicker({ locale: 'fr', format: "DD/MM/YYYY HH:mm" });
+        //$(this.inputElementGroup).on("dp.change", this.handleChangeJsEventGlobal);
     }    renderField() {
-        return <div className='input-group datetime' ref={(input) => { this.inputElementGroup = input; } } >
-            <input
-                ref={(input) => { this.inputElement = input; } }
-                style={this.state.hasError === true ? { borderColor: "red" } : null}
-                type='text'
-                className="form-control" />
-            <span className="input-group-addon">
-                <span className="glyphicon glyphicon-calendar"></span>
-            </span>
-        </div>
+        return <UpDateTime onChange={this.handleChangeEventGlobal} hasError={this.state.hasError} isNuallble={this.isNuallble}></UpDateTime>
+
+        //return <div className='input-group datetime' ref={(input) => { this.inputElementGroup = input; } } >
+        //    <input
+        //        ref={(input) => { this.inputElement = input; } }
+        //        style={this.state.hasError === true ? { borderColor: "red" } : null}
+        //        type='text'
+        //        className="form-control" />
+        //    <span className="input-group-addon">
+        //        <span className="glyphicon glyphicon-calendar"></span>
+        //    </span>
+        //</div>
     }
 
 
-    handleChangeJsEvent(event: any) {
-        if (typeof (event.date) === "object" && event.date && typeof (event.date.toDate) === "function") {
-            return event.date.toDate();
-        }
-        return null;
+    handleChangeJsEvent(event: Date) {
+        //if (typeof (event.date) === "object" && event.date && typeof (event.date.toDate) === "function") {
+        //    return event.date.toDate();
+        //}
+        //return null;
+        return event;
     }
 
     isEmpty(value) {
---
+++ src/externalReactComponent/UpDateTime.tsx
@@ -0,0 +1,54 @@
+﻿import * as $ from "jquery";
+import "eonasdan-bootstrap-datetimepicker"
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { UpFormControl } from "../UpForm/UpFormControl"
+import { TypeDateControl, dateFormat } from "../ControlError/TypeDateControl"
+
+
+interface UpDateTimeProps {
+    hasError: boolean;
+    onChange: (value?: Date) => void;
+    isNuallble: boolean;
+    default?: Date;
+}
+
+interface UpDateTimeState {
+    value?: Date;
+}
+
+export default class UpDateTime extends React.Component<UpDateTimeProps, UpDateTimeState> {
+    inputElementGroup: HTMLDivElement;    constructor(p, c) {        super(p, c);
+    }    setInput(data) {        $(this.inputElementGroup).data("DateTimePicker").date(data);    }    componentDidMount() {
+        $(this.inputElementGroup).datetimepicker({ locale: 'fr', format: "DD/MM/YYYY HH:mm" });
+        $(this.inputElementGroup).on("dp.change", this.handleChangeJsEvent);
+    }    render() {
+
+        return <div className='input-group date' style={{ marginBottom: "3px" }} ref={(input) => { this.inputElementGroup = input; }} >
+            <input
+                //ref={(input) => { this.inputElement = input; } }
+                style={this.props.hasError === true ? { borderColor: "red" } : null}
+                type='text'
+                className="form-control" />
+            <span className="input-group-addon">
+                <span className="glyphicon glyphicon-calendar"></span>
+            </span>
+        </div>
+
+    }
+
+    handleChangeJsEvent(event: any) {
+        if (typeof (event.date) === "object" && event.date && typeof (event.date.toDate) === "function") {
+            this.setState({ value: event.date.toDate() }, this.dispatchOnChange)
+            return
+        }
+        this.setState({ value: null }, this.dispatchOnChange)
+    }
+
+    dispatchOnChange = () => {        this.props.onChange(this.state.value);
+    }
+    isEmpty(value) {
+        return value === null || value === undefined || value === "";
+    }
+}
+
