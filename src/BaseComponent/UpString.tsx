a246eb1968c073bbf135be17a76d3cf81316064d
SunTzu <171946@supinfo.com>
2017-04-19 14:40:01 +0200

    les pattern sont dans les composant

--- src/BaseComponent/UpString.tsx
+++ src/BaseComponent/UpString.tsx
@@ -7,30 +7,12 @@ export default class UpString extends UpFormControl<string> {
 
     constructor(p, c) {
         super(p, c);
-
         var pattern = new RegExp(this.props.schema.pattern);
         var patternErrorMessage = this.props.schema.patternErrorMessage;
-
-        if (this.props.schema.format !== undefined) {
-            switch (this.props.schema.format) {
-                case "email":
-                    pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                    patternErrorMessage = "Doit être mail";
-                    break;
-                case "phone":
-                    pattern = /^(0)[1-9]([\s]?[0-9]{2}){4}$/
-                    patternErrorMessage = "Doit être un N° de telephone";
-                    break;
-                default:
-            }
-        }
-        this._ControlErrorCentral.addControl(new TypeStringControl(pattern, patternErrorMessage));
     }
 
     setInput(data) {
-        if (this.inputElement) {
-            this.inputElement.value = data;
-        }
+     
     }
 
     renderField() {
