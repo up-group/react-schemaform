ebb55b7d14c338bda95ab03bb9041d49dd6e9364
SunTzu <171946@supinfo.com>
2017-04-28 11:08:25 +0200

    gestion des dates min/max

--- src/BaseComponent/UpDate.tsx
+++ src/BaseComponent/UpDate.tsx
@@ -13,7 +13,17 @@ export default class UpDateComp extends UpFormControl<Date> {
     }
 
     renderField() {
-        return <UpDate value={this.state.value} onChange={this.handleChangeEventGlobal} />
+        var maxDate: Date, minDate: Date;
+
+        if (this.props.schema.maximum !== undefined) {
+            maxDate = new Date(this.props.schema.maximum);
+        }
+
+        if (this.props.schema.minimum !== undefined) {
+            minDate = new Date(this.props.schema.minimum);
+        }
+
+        return <UpDate maxDate={maxDate} minDate={minDate} onChange={this.handleChangeEventGlobal} />
     }
 
 
--- src/BaseComponent/UpDateTime.tsx
+++ src/BaseComponent/UpDateTime.tsx
@@ -13,7 +13,17 @@ export default class UpDateTimeComp extends UpFormControl<Date> {
     }
 
     renderField() {
-        return <UpDateTime onChange={this.handleChangeEventGlobal} />
+        var maxDate: Date, minDate: Date;
+
+        if (this.props.schema.maximum !== undefined) {
+            maxDate = new Date(this.props.schema.maximum);
+        }
+
+        if (this.props.schema.minimum !== undefined) {
+            minDate = new Date(this.props.schema.minimum);
+        }
+
+        return <UpDateTime maxDate={maxDate} minDate={minDate} onChange={this.handleChangeEventGlobal} />
     }
 
 }
