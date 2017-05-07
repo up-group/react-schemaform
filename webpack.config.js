b71351ad05e5d1dad3fa443ab2e3f4a75641646d
Wicusiu <guillaume.chomat@arcan.fr>
2017-05-07 08:15:58 +0200

    Fix fonts error

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+const ROOT_PATH = path.resolve(__dirname);
+
 module.exports = {
-    entry: {
-        'docs/dist/demo': './docs/demo.tsx'
-    },
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-hot-middleware/client',
+        './docs/demo.tsx'
+    ],
     output: {
-        filename: '[name].js'
-    }          ,
-    devtool: "source-map",
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'demo.js',
+        publicPath: '/',
+    },      
+    devtool: 'cheap-module-source-map',
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js", ".css"]
     },
     module: {
         rules: [
-            {
-                test: /\.tsx?|.ts?$/,
-                use: [
-                   
-                     'ts-loader'
-                ]
-            },
-            { test: /\.css$/, loader: 'style-loader!css-loader?url=false' },
+            { test: /\.tsx?$/, loader: 'ts-loader' },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
             { test: /\.json$/, loader: 'json-loader' },
             { test: /\.md$/, loader: 'html!markdown-loader' },
-            { test: /\.svg$/, loader: 'svg-inline-loader' },
-            { test: /\.(eot|ttf|woff|woff2)$/, loader: 'file-loader' }
+            { test: /\.svg$/, loader: 'svg-inline-loader'},
+            {
+                test: /\.(eot|ttf|woff|woff2)\??/,
+                loader: 'file-loader?name=fonts/[name].[ext]'
+            }
         ]
-    }
-
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ]
 };
