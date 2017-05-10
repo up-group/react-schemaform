1c763d85ba8b406834c5a86bbc9298c648c09e7e
SunTzu <171946@supinfo.com>
2017-05-10 12:17:21 +0200

    temp : retour sur webpack precedent

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,30 @@
-const webpack = require('webpack');
-const path = require('path');
-const ROOT_PATH = path.resolve(__dirname);
-
 module.exports = {
-    entry: [
-        'react-hot-loader/patch',
-        'webpack-hot-middleware/client',
-        './docs/demo.tsx'
-    ],
+    entry: {
+        'docs/dist/demo': './docs/demo.tsx'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'demo.js',
-        publicPath: '/',
-    },      
-    devtool: 'cheap-module-source-map',
+        filename: '[name].js'
+    },
+    devtool: "source-map",
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js", ".css"]
     },
     module: {
         rules: [
-            { test: /\.tsx?$/, loader: 'ts-loader' },
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            {
+                test: /\.tsx?|.ts?$/,
+                use: [
+
+                    'ts-loader'
+                ]
+            },
+            { test: /\.css$/, loader: 'style-loader!css-loader?url=false' },
             { test: /\.json$/, loader: 'json-loader' },
             { test: /\.md$/, loader: 'html!markdown-loader' },
-            { test: /\.svg$/, loader: 'svg-inline-loader'},
-            {
-                test: /\.(eot|ttf|woff|woff2)\??/,
-                loader: 'file-loader?name=fonts/[name].[ext]'
-            }
+            { test: /\.svg$/, loader: 'svg-inline-loader' },
+            { test: /\.(eot|ttf|woff|woff2)$/, loader: 'file-loader' }
         ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-    ]
+    }
+
 };
