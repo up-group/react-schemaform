74cd83bf9fea76a73b272325798681ac03dc470b
SunTzu <171946@supinfo.com>
2017-07-28 16:43:26 +0200

    check null value

--- src/UpForm/UpSchemaObject.tsx
+++ src/UpForm/UpSchemaObject.tsx
@@ -29,8 +29,9 @@ export default class UpSchemaObject extends React.Component<UpSchemaObjectProps,
             }
         }
         var elements = properties.map((property, index) => {
+            var value = this.props.initData == null ? undefined : this.props.initData[propertiesName[index]];
             return (<UpCol key={index} span={this.sizeSpan(property)}><UpSchemaFormComponentSelector
-                initData={this.props.initData[propertiesName[index]]}
+                initData={value}
                 showError={this.props.showError}
                 isRequired={this.isRequired(propertiesName[index])}
                 key={index}
