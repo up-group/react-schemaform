506e933aefbdc4fa9ff21870c27981aa3a47d61b
attef touina <attef.touina@up.coop>
2019-11-22 11:14:22 +0100

    migrations of @up-group/react-controls and typescript

--- docs/demo.tsx
+++ docs/demo.tsx
@@ -14,6 +14,7 @@ import "jsoneditor-react/es/editor.min.css";
 
 import JSONInput from "react-json-editor-ajrm";
 import locale from "react-json-editor-ajrm/locale/en";
+import { JsonSchemaHelper } from "../src";
 
 //ajv.addMetaSchema(require("ajv/lib/refs/json-schema-draft-07.json"));
 
@@ -55,10 +56,58 @@ class Demo extends React.Component<{}, DemoState> {
     };
   }
 
+  removeUnSupportedFeatures(argumentsSchema: JsonSchema) {
+    for (const key in argumentsSchema.definitions) {
+        if (argumentsSchema.definitions.hasOwnProperty(key)) {
+            const element = argumentsSchema.definitions[key];
+            if (element.format === "date-time")
+                element.format = null
+            this.removeUnSupportedFeatures(element)
+        }
+    }
+    for (const key in argumentsSchema.properties) {
+        if (argumentsSchema.properties.hasOwnProperty(key)) {
+            const element = argumentsSchema.properties[key];
+            if (element.type === undefined) {
+                delete argumentsSchema.properties[key];
+            } else {
+                if (element.format === "date-time") element.format = null;
+                argumentsSchema.properties[key].title = key;
+                this.removeUnSupportedFeatures(element);
+            }
+        }
+    }
+  }
+
+componentDidMount(){
+  fetch("http://localhost:5000/api/v2/subscriptiontheme/metadatas/create")
+  .then(response => {
+      if (!response.ok) {
+          throw new Error(response.statusText)
+      }
+      return response.json();
+  })
+  .then(metadata => {
+      if (metadata == null) {
+          return;
+      }
+      var argumentsSchema = JsonSchemaHelper.parseSchema(metadata.argumentsSchema);
+      this.removeUnSupportedFeatures(argumentsSchema)
+      this.setState({
+        schema : argumentsSchema
+      })
+  })
+}
   render() {
     return (
       <UpThemeProvider theme={UpDefaultTheme}>
         <>
+          <UpSchemaForm
+            initValue={{ size: this.state.nb }}
+            showError={this.state.showError}
+            schema={this.state.schema}
+            onFormChange={this.onFormPayload}
+          />
           <JSONInput
             id="a_unique_id"
             placeholder={this.state.schema}
@@ -67,14 +116,7 @@ class Demo extends React.Component<{}, DemoState> {
             colors={{
               string: "#DAA520" // overrides theme colors with whatever color value you want
             }}
-            height="550px"
             onChange={this.onSchemaChange}
-          />
-          <UpSchemaForm
-            initValue={{ size: this.state.nb }}
-            showError={this.state.showError}
-            schema={this.state.schema}
-            onFormChange={this.onFormPayload}
           />
         </>
       </UpThemeProvider>
