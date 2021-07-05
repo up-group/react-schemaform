b558b63202b21560c21dbba873dba793b4f92ec1
DESKTOP-5BOU3QA\admin <amine.allouti.ext@up.coop>
2021-07-05 23:11:56 +0200

    Ajouter la possibilité de passer un objet pour appliquer un style sur les types number, integer et string

--- src/BaseComponent/NumberField.tsx
+++ src/BaseComponent/NumberField.tsx
@@ -1,6 +1,7 @@
 ﻿import * as React from "react";
 import { UpFormControl } from "../UpForm/UpFormControl";
 import { UpNumber } from "@up-group-ui/react-controls";
+import { style } from "typestyle";
 
 export default class NumberField extends UpFormControl<number> {
   constructor(p, c) {
@@ -8,10 +9,11 @@ export default class NumberField extends UpFormControl<number> {
   }
 
   renderField() {
-    const { additionalProps: { hideButtons } } = this.props;
+    const { additionalProps: { hideButtons, objectStyles } = {} } = this.props;
 
     return (
       <UpNumber
+        className={objectStyles && style(objectStyles)}
         name={this.props.name}
         value={this.state.value}
         showError={this.props.showError}
--- src/UpForm/UpSchemaFormComponentSelector.tsx
+++ src/UpForm/UpSchemaFormComponentSelector.tsx
@@ -58,10 +58,10 @@ export default class UpSchemaFormComponentSelector extends React.Component<
     }
 
     private findGetParameter(parameterName) {
-        var result = null,
+        let result = null,
             tmp = [];
-        var items = location.search.substr(1).split("&");
-        for (var index = 0; index < items.length; index++) {
+        const items = location.search.substr(1).split("&");
+        for (let index = 0; index < items.length; index++) {
             tmp = items[index].split("=");
             if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
         }
