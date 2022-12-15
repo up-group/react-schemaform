6deb21da09dd3c25b0ed5b593e135ef3d11ca76b
chomatguillaume <guillaume.chomat@up.coop>
2022-12-15 13:20:31 +0100

    Handle TagSelect value

--- src/BaseComponent/TagsSelect.tsx
+++ src/BaseComponent/TagsSelect.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 import { UpFormControl } from "../UpForm/UpFormControl";
 import { UpTagsSelect } from "@up-group-ui/react-controls";
-import { TagData } from "@up-group-ui/react-controls/dist/Components/Inputs/TagsSelect";
-import { SelectedTagData } from "@up-group-ui/react-controls/dist/Components/Display/Tag";
 import { JsonSchema } from "interfaces/JsonSchema";
+import { TagData } from "@up-group-ui/react-controls/dist/Components/Display/Tag/UpTag";
 export interface ExtendedProps {
     label?: string;
 }
 
-export default class TagsSelect extends UpFormControl<SelectedTagData[], ExtendedProps> {
+export default class TagsSelect extends UpFormControl<TagData[], ExtendedProps> {
 
     constructor(p, c) {
         super(p, c);
@@ -19,14 +18,15 @@ export default class TagsSelect extends UpFormControl<SelectedTagData[], Extende
     
     renderField() {
         const { label } = this.props;
-
-        let tags = [];
+ 
+        let tags : TagData[] = [];
         for (let i = 0; i < this.schema.enum.length; i++) {
           if (this.schema.enum[i] !== null) {
             if (this.schema.hiddenEnumValues == null || this.schema.hiddenEnumValues.indexOf(this.schema.enum[i]) === -1 )
               tags.push({
                 id: this.schema.enum[i],
-                text: this.props.translate ? this.props.translate(this.schema.enumDescriptions[i]) : this.schema.enumDescriptions[i]
+                text: this.props.translate ? this.props.translate(this.schema.enumDescriptions[i]) : this.schema.enumDescriptions[i],
+                selected: this.props.value && this.props.value.some(value => value.id === this.schema.enum[i])
               });
           }
         }
